666c81772f98ed856333ca2169452daca44aff92
YAYAYru <AlexeyAYAYA@gmail.com>
2023-01-17 17:07:02 +0700

    Код работает

--- src/model1.py
+++ src/model1.py
@@ -13,6 +13,17 @@ PATH_NPY_X_TEST = PATH_FOLDER + "x_test.npy"
 PATH_NPY_Y_TRAIN = PATH_FOLDER + "y_train_1.npy"
 PATH_NPY_Y_TEST = PATH_FOLDER + "y_test_1.npy"
 
+x_train = np.load(PATH_NPY_X_TRAIN)
+x_test = np.load(PATH_NPY_X_TEST)
+y_train = np.load(PATH_NPY_Y_TRAIN)
+y_test = np.load(PATH_NPY_Y_TEST)
+
+print("x_train.shape", x_train.shape)
+print("x_test.shape", x_test.shape)
+print("y_train.shape", y_train.shape)
+print("y_test.shape", y_test.shape)
+
+
 # https://stackoverflow.com/questions/36288235/how-to-get-stable-results-with-tensorflow-setting-random-seed
 SEED = 0
 def set_seeds(seed=SEED):
@@ -30,6 +41,28 @@ def set_global_determinism(seed=SEED):
 set_global_determinism(seed=SEED)
 
 
+def create_task_learning_model(x_train_shape, y_train_shape):
+
+    inputs = tf.keras.layers.Input(shape=(x_train_shape[1], x_train_shape[2], x_train_shape[3]), name='input')
+
+    main_branch = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1)(inputs)
+    main_branch = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(main_branch)
+    main_branch = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1)(main_branch)
+    main_branch = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(main_branch)
+    main_branch = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), strides=1)(main_branch)
+    main_branch = tf.keras.layers.Flatten()(main_branch)
+    main_branch = tf.keras.layers.Dense(3512, activation='relu')(main_branch)
+
+    task_1_branch = tf.keras.layers.Dense(1024, activation='relu')(main_branch)
+    task_1_branch = tf.keras.layers.Dense(512, activation='relu')(task_1_branch)
+    task_1_branch = tf.keras.layers.Dense(256, activation='relu')(task_1_branch)
+    task_1_branch = tf.keras.layers.Dense(128, activation='relu')(task_1_branch)
+    task_1_branch = tf.keras.layers.Dense( y_train_shape[1], activation='softmax')(task_1_branch)
+
+    model = tf.keras.Model(inputs = inputs, outputs = [task_1_branch])
+    model.summary()
+    return model
+
 experiment = mlflow.set_experiment("model1")
 
 # Get Experiment Details
@@ -41,78 +74,37 @@ print("Lifecycle_stage: {}".format(experiment.lifecycle_stage))
 with mlflow.start_run() as run:
     mlflow.tensorflow.autolog()
 
-    x_train = np.load(PATH_NPY_X_TRAIN)
-    x_test = np.load(PATH_NPY_X_TEST)
-    y_train = np.load(PATH_NPY_Y_TRAIN)
-    y_test = np.load(PATH_NPY_Y_TEST)
+    print('Starting training on batch of models for multitasks ', '\n\n')
+    
+    model = create_task_learning_model(x_train.shape, y_train.shape)
+    model.compile(optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
 
-    print("x_train.shape", x_train.shape)
-    print("x_test.shape", x_test.shape)
-    print("y_train.shape", y_train.shape)
-    print("y_test.shape", y_test.shape)
+    start = time.time()
+    model2_history = model.fit(x_train, y_train,
+                        epochs=15, batch_size=128, verbose=0)
 
-    def create_task_learning_model(x_train_shape, y_train_shape):
+    print(f'Training time: {time.time() - start}\n')
 
-        inputs = tf.keras.layers.Input(shape=(x_train_shape[1], x_train_shape[2], x_train_shape[3]), name='input')
-
-        main_branch = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1)(inputs)
-        main_branch = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(main_branch)
-        main_branch = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), strides=1)(main_branch)
-        main_branch = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=2)(main_branch)
-        main_branch = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), strides=1)(main_branch)
-        main_branch = tf.keras.layers.Flatten()(main_branch)
-        main_branch = tf.keras.layers.Dense(3512, activation='relu')(main_branch)
-
-        task_1_branch = tf.keras.layers.Dense(1024, activation='relu')(main_branch)
-        task_1_branch = tf.keras.layers.Dense(512, activation='relu')(task_1_branch)
-        task_1_branch = tf.keras.layers.Dense(256, activation='relu')(task_1_branch)
-        task_1_branch = tf.keras.layers.Dense(128, activation='relu')(task_1_branch)
-        task_1_branch = tf.keras.layers.Dense( y_train_shape[1], activation='softmax')(task_1_branch)
-
-        model = tf.keras.Model(inputs = inputs, outputs = [task_1_branch])
-        model.summary()
-        return model
-
-
-    def compile_task_model(model):
-        model.compile(optimizer='adam',
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
-        return model
-
-
-    def fit_batch():
-        print('Starting training on batch of models for multitasks ', '\n\n')
-        
-        model = create_task_learning_model(x_train.shape, y_train.shape)
-        model = compile_task_model(model)
-
-        start = time.time()
-        model2_history = model.fit(x_train, y_train,
-                            epochs=15, batch_size=128, verbose=0)
-
-        print(f'Training time: {time.time() - start}\n')
-        return model2_history, model
-            
-
-    training_history, trained_model = fit_batch()
-    trained_model.save(PATH_MODEL)
+    model.save(PATH_MODEL)
 
     new_model = tf.keras.models.load_model(PATH_MODEL)
     new_model.summary()
 
-    e = trained_model.evaluate(x_test, y_test)
+    e = model.evaluate(x_test, y_test)
     mlflow.log_metric("test_loss", e[0])
     print('Task1 evaluate test loss: ', e[0])
     mlflow.log_metric("test_acc", e[1])
     print('Task1 evaluate test acc: ', e[1])
 
-    # model_info = mlflow.tensorflow.log_model(
-    #     model=trained_model, 
-    #     artifact_path="model1",
-    #     registered_model_name="model1",
-    #     )
-    model_uri = "runs:/{}/model1".format(run.info.run_id)
-    mv = mlflow.register_model(model_uri, "model1")
-    print("Name: {}".format(mv.name))
-    print("Version: {}".format(mv.version))
+    # model_uri = "runs:/{}/model1".format(run.info.run_id)
+    # mv = mlflow.register_model(model_uri, "model1")
+    # print("Name: {}".format(mv.name))
+    # print("Version: {}".format(mv.version))
+
+# mlflow models serve --no-conda -m file:///home/yayay/yayay/git/github/paper_mlflow/src/mlruns/902157297686484746/dcfc070aae044571af6577fa8f2f88b2/artifacts/model -h 0.0.0.0 -p 8001
+
+# export CUDA_VISIBLE_DEVICES='' Выключить GPU для предикта
+# export CUDA_VISIBLE_DEVICES='0' Включить GPU для обучения
+# https://datascience.stackexchange.com/questions/58845/how-to-disable-gpu-with-tensorflow
